d169ac703b83ebe889e22e31ab8abfe74fd55794
Jelle van Wieringen <jellevanwieringen@mailbox.org>
2024-12-21 13:24:27 +0100

    Clickdummy SOFT

--- src/dt_prototype/ui/sample_proto.py
+++ src/dt_prototype/ui/sample_proto.py
@@ -1,15 +1,23 @@
 import gradio as gr
 
 
-def store_message(message: str, history: list[str]):
-    output = {
-        "Current messages": message,
-        "Previous messages": history[::-1]
-    }
-    history.append(message)
-    return output, history
+with gr.Blocks() as demo:
+    title = gr.HTML("<H1>Long Range Planning with SOFT</H1>")
+    with gr.Row():
+        issue = gr.TextArea(label="Issue Editor")
+        suggested = gr.TextArea(label="Proposed by AI through corrections based on the validation report")
+    validator = gr.TextArea(label="Validation report on your Issue Editor text")
+    with gr.Row():
+        validate_button = gr.Button("Validate")
+        issue_button = gr.Button("Issue yours")
+        issue_correction_button = gr.Button("Issue proposed")
 
 
-demo = gr.Interface(fn=store_message,
-                    inputs=["textbox", gr.State(value=[])],
-                    outputs=["json", gr.State()])
+    @validate_button.click(inputs=issue, outputs=validator)
+    def validate_issue(issue_text: str):
+        return issue_text + "Valid"
+
+
+    @validate_button.click(inputs=issue, outputs=suggested)
+    def issue_correction(issue_text: str):
+        return issue_text + "Correction"
